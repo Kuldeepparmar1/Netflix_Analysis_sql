aaaec81b6bc676c33684cea84de9b3b13a1a7701
Kuldeep Parmar <135144769+Kuldeepparmar1@users.noreply.github.com>
2025-03-01 12:01:24 +0530

    Query.sql

--- Query.sql
+++ Query.sql
@@ -1,26 +1,3 @@
-Drop table if exists netflix;
-CREATE  TABLE netflix
-(
-	show_id	VARCHAR(6),
-	type VARCHAR(10),
-	title VARCHAR(150),
-	director VARCHAR(210),
-	casts VARCHAR(1000),
-	country VARCHAR(150),
-	date_added VARCHAR(50),
-	release_year INT,
-	rating VARCHAR(10),
-	duration VARCHAR(15),
-	listed_in VARCHAR(150),
-	description VARCHAR(250)
-);
-SELECT * FROM NETFLIX;
-
-select 
-    count(*) 
-from netflix;
-
-select distinct type from netflix;
 
 -- 1. Count the number of Movies vs TV Shows
 
